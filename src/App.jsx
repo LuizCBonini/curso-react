e9eef2fada51b2023ae7a149bf35cc6cc83e3dfe
Luiz Bonini <lc.bonini13@gmail.com>
2022-03-03 20:28:45 -0300

    Função tarefa concluída.

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,14 @@ const App = () => {
     }
   ])
 
+  const handleTaskClick = (TaskId) => {
+    const newTasks = tasks.map(task => {
+      if (task.id === TaskId) return { ...task, completed: !task.completed}
+      return task;
+    })
+    setTasks(newTasks)
+  }
+
   const handleTaskAddition = (taskTitle) => {
     const newTasks = [... tasks, {
       title: taskTitle,
@@ -35,8 +43,8 @@ const App = () => {
   return (
     <>
       <div className='container'>
-        <AddTask handleTaskAddition={handleTaskAddition}/>
-        <Tasks tasks={tasks}/>
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
       </div>
     </>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Task from "./Task";
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, handleTaskClick}) => {
     return (
         
         //Para cada Task, renderizara um Task
         <>
-            {tasks.map(task => <Task task={task}/>)}
+            {tasks.map(task => (
+                <Task task={task} handleTaskClick={handleTaskClick}/>
+            ))}
         </>
 
     )
